f0563b60b8e501b84629f75460ed979d83e2fd2d
KaterinaSokol <rinasokol17@gmail.com>
2022-09-12 10:34:23 +0300

    update css & js files

--- script.js
+++ script.js
@@ -27,11 +27,14 @@ function showWeather(response){
     let humidityElement = document.querySelector("#humidity");
     let descroptionElement = document.querySelector("h5");
     let temperatureElement = document.querySelector("#today");
+    let iconElement = document.querySelector("#icon");
     
     temperatureElement.innerHTML = `${temperature}`;
     descroptionElement.innerHTML = `${description}`;
     windSpeedElement.innerHTML = `Wind: ${windSpeed} km/h`;
     humidityElement.innerHTML = `Humidity: ${humidity}%`;
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 
     document.querySelector("#city-name").innerHTML = response.data.name;
 
