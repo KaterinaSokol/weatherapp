ba500349163127ed917cef3a40564e9c46f56e44
KaterinaSokol <rinasokol17@gmail.com>
2022-09-14 18:34:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,8 @@ if(hours <10){
 let changeInformation = document.querySelector("#date-time");
 changeInformation.innerHTML = `${day}, ${hours}:${minutes}`;
 
-function displayForecast(){
+function displayForecast(response){
+    console.log(response.data.daily);
     let forecastElement = document.querySelector("#forecast");
 
     let forecastHTML = `<div class="row">`;
@@ -41,13 +42,13 @@ function displayForecast(){
     forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
 }
-displayForecast();
 
 function getForecast(coordinates) {
     console.log(coordinates);
-    let apiKey = "e7dbb1a7cbf271831b301595cd51d95b";
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    let apiKey = "85bbd3d16a2dfe0ecf253c7ae1e8fe03";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     console.log(apiUrl);
+    axios.get(apiUrl).then(displayForecast);
 }
 
 function showWeather(response){
